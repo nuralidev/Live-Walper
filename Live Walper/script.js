beb4da3caccf08ae13d9df8a2fb6aaedc677e181
nuralidev <nuralimammadovis@gmail.com>
2024-07-13 23:16:15 +0400

    Update 1.6

--- Live Walper/script.js
+++ Live Walper/script.js
@@ -2,15 +2,28 @@ const clock = document.querySelector(".clock")
 
 const trick = () => {
     const newDate = new Date()
-    const saat=newDate.getHours()
-    const deqiqe=newDate.getMinutes()
-    const saniye=newDate.getSeconds()
-   const html=`
+    let saat = newDate.getHours()
+    let deqiqe = newDate.getMinutes()
+    let saniye = newDate.getSeconds()
+    if (saat< 10) {
+        saat = "0" + saat
+
+    }
+    if (deqiqe < 10) {
+        deqiqe= "0" + deqiqe
+
+    }
+    if (saniye < 10) {
+        saniye = "0" + saniye
+
+    }
+    const html = `
    <span class="new">${saat}:</span>
    <span class="new" >${deqiqe}:</span>
    <span class="new" >${saniye}</span>
    `
-   clock.innerHTML=html
+    clock.innerHTML = html
+    console.log(saniye)
 }
 
 setInterval(trick, 1000)
